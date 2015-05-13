4f38f9eb702a0cae575771d368316052a8f21162
000935032 <000935032@N215A4.labs.neit.edu>
2015-05-12 22:42:47 -0400

    More Progress

--- Week3/lab3/site/mvc/controllers/EmailController.php
+++ Week3/lab3/site/mvc/controllers/EmailController.php
@@ -1,33 +1,56 @@
 <?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 /**
  * Description of EmailController
  *
- * @author Nick
+ * @author User
  */
+
 namespace APP\controller;
+
 use App\models\interfaces\IController;
+use App\models\services\Scope;
 use App\models\interfaces\IService;
+use App\models\interfaces\IModel;
+
 class EmailController extends BaseController implements IController {
-   
-    protected $service;
-    
-    public function __construct( IService $EmailService  ) {                
-        $this->service = $EmailService;  
+
+    /**
+     * @var IService
+     */
+  
+
+    public function __construct( IService $EmailService, IModel $model  ) {                
+        $this->service = $EmailService;     
+        $this->data['model'] = $model;
     }
-    
-    public function execute(IService $scope) {
+
+
+    public function execute(Scope $scope) {
+                
+        $this->data['model']->reset();
         $viewPage = 'email';
         
-        $this->data['model'] = $this->service->getNewEmailModel();
-        $this->data['model']->reset();
         
         if ( $scope->util->isPostRequest() ) {
-            
             
             if ( $scope->util->getAction() == 'create' ) {
                 $this->data['model']->map($scope->util->getPostValues());
                 $this->data["errors"] = $this->service->validate($this->data['model']);
                 $this->data["saved"] = $this->service->create($this->data['model']);
+            }
+            
+            if ( $scope->util->getAction() == 'update'  ) {
+                $this->data['model']->map($scope->util->getPostValues());
+                $this->data["errors"] = $this->service->validate($this->data['model']);
+                $this->data["updated"] = $this->service->update($this->data['model']);
+                 $viewPage .= 'edit';
             }
             
             if ( $scope->util->getAction() == 'edit' ) {
@@ -39,19 +62,17 @@ class EmailController extends BaseController implements IController {
             if ( $scope->util->getAction() == 'delete' ) {                
                 $this->data["deleted"] = $this->service->delete($scope->util->getPostParam('emailid'));
             }
-            
-             if ( $scope->util->getAction() == 'update'  ) {
-                $this->data['model']->map($scope->util->getPostValues());
-                $this->data["errors"] = $this->service->validate($this->data['model']);
-                $this->data["updated"] = $this->service->update($this->data['model']);
-                 $viewPage .= 'edit';
-            }   
+                       
         }
-         
-        $this->data['emailTypes'] = $this->service->getAllEmailTypes(); 
-        $this->data['emails'] = $this->service->getAllEmails(); 
+        
+        
+       
+        $this->data['Emails'] = $this->service->getAllRows();        
+        
         
         $scope->view = $this->data;
         return $this->view($viewPage,$scope);
-    } 
+    }
+    
+    
 }
